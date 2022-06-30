fcd08ec90a244ec3d7c6a45d1b07f6153c2631db
蔡孟辰 <b06502020@ntu.edu.tw>
2022-06-30 19:25:25 +0200

    bug fixed

--- FizzBuzz/main.cpp
+++ FizzBuzz/main.cpp
@@ -2,20 +2,28 @@
 #include <string>
 
 
-int main (int argc, char* argv[]){
+int main (){
     // Get inputs
-    std::cout << argv[0] << std::endl;
-    std::string x_string = argv[1];
-    std::string y_string = argv[2];
-    std::string n_string = argv[3];
-    int X = std::stoi(x_string);
-    int Y = std::stoi(y_string);
-    int N = std::stoi(n_string);
+    std::string x{}; 
+    std::cin >> x;
+    std::string y{};
+    std::cin >> y;
+    std::string z{};
+    std::cin >> z;
+    int X = std::stoi(x);
+    int Y = std::stoi(y);
+    int N = std::stoi(z);
 
-    // 4 cases in loop
+    // 5 cases in loop
     for (size_t i{1}; i <= N; ++i){
         // 1 is special (always divisible)
-        if (i == 1) std::cout << 1 << std::endl;
+        if (i == 1) {
+            if (X == 1){
+                std::cout << "Fizz" << std::endl;
+            } else{
+                std::cout << 1 << std::endl;
+            }
+        }
         // FizzBuzz (both X and Y)
         else if (i % X == 0 && i % Y == 0){
             std::cout << "FizzBuzz" << std::endl;
